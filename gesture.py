33c8ae7c878c2fda1ae52b0fd8755df8f9188473
ZKod <zkod777@gmail.com>
2022-08-23 11:04:00 +0400

    direction controlled by thumb

--- gesture.py
+++ gesture.py
@@ -1,8 +1,11 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
+import math
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+direction = "left"
 while True:
     # Get image frame
     success, img = cap.read()
@@ -11,14 +14,26 @@ while True:
     # hands = detector.findHands(img, draw=False)  # without draw
 
     if hands:
-        # Hand 1
-        hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmark points
-        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
-        centerPoint1 = hand1['center']  # center of the hand cx,cy
-        handType1 = hand1["type"]  # Handtype Left or Right
+        hand = hands[0]
+        if hand["type"] == "Right":
+            thumbBase = hand["lmList"][1]
+            thumbTip = hand["lmList"][4]
+            angle = math.atan2(thumbTip[1] - thumbBase[1], thumbTip[0] - thumbBase[0]) * 180 / math.pi
 
-        fingers1 = detector.fingersUp(hand1)
+            newDirection = None
+            # bottom left quadrant
+            if angle > 20 and angle < 60:
+                newDirection = "down"
+            elif angle < -10 and angle > -40:
+                newDirection = "left"
+            elif angle < -40 and angle > -80:
+                newDirection = "up"
+            elif angle < -100 and angle > -150:
+                newDirection = "right"
+            
+            if newDirection != None and newDirection != direction:
+                direction = newDirection
+                print(f"Direction changed to: {direction}")
 
     # Display
     cv2.imshow("Preview", img)
